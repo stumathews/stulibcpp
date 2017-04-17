8d9a55432f516f1689a00294b40b840b60a1b398
Stuart <stumathews@gmail.com>
2017-04-17 11:20:42 +0100

    use c++11

--- tests/testlib.cpp
+++ tests/testlib.cpp
@@ -1,9 +1,21 @@
 #include <libstucpp.h>
+#include <iostream>
+#include <vector>
+
 
 int main(int argc, char** argv)
 {
 	test();
-	TestClass t;
+	TestClass t;	
 	t.one();
+
+	std::vector<std::string> strings;
+	using StringList = std::vector<std::string>;
+	
+	StringList::iterator it;
+	for (it = strings.begin(); it != strings.end(); it++)
+	{
+		std::cout << it->c_str() << std::endl;
+	}
 	return 0;
 }
