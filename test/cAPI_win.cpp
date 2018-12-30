8df35b0543804330fd707e938918488ab0c87011
Stuart Mathews <stumathews@gmail.com>
2018-12-30 19:11:30 +0000

    Update cAPI_win.cpp

--- test/cAPI_win.cpp
+++ test/cAPI_win.cpp
@@ -1,11 +1,12 @@
 #include <windows.h>
 #include <iostream>
 
-typedef void* (__stdcall *INIT)();
-typedef void (__stdcall *END)();
+typedef void* ( *INIT)();
+typedef void ( *END)();
+typedef int ( *SUB)(int,int);
 
 int main() {
-    HINSTANCE lib = LoadLibrary("libstucpp.dll");
+    HINSTANCE lib = LoadLibrary("stucpp.dll");
 
     if (lib == NULL) {
         std::cerr << "cannot locate the .dll file" << std::endl;
@@ -13,13 +14,14 @@ int main() {
     }
     INIT init = (INIT)GetProcAddress(lib, "initLib");
     END end = (END)GetProcAddress(lib, "closeLib");
+	SUB sub = (SUB)GetProcAddress(lib, "sub");
     if (!init || !end)
     {
         std::cerr << "Couldn't find function" << std::endl;
         return 1;
     }
     init();
-
+	std::cout << "sub(10,5) = " << sub(10,5) << std::endl;
     end();
     return 0;
 }
